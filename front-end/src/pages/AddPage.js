f3b603133fe9b368bf040d2f49931745657997fb
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-03 14:37:49 -0300

    modificacion de prettier

--- front-end/src/pages/AddPage.js
+++ front-end/src/pages/AddPage.js
@@ -1,30 +1,30 @@
-import Header from '../containers/Header/Header';
-import AddForm from '../containers/Form/Form';
-import ResumeTable from '../containers/ResumeTable/ResumeTable';
-import H1 from '../components/H1/H1';
+import Header from "../containers/Header/Header";
+import AddForm from "../containers/Form/Form";
+import ResumeTable from "../containers/ResumeTable/ResumeTable";
+import H1 from "../components/H1/H1";
 
 const AddPage = ({ sumitHandler }) => {
   let isIncome;
 
   const clickHandler = (event) => {
-    const form = event.target.closest('form');
+    const form = event.target.closest("form");
 
-    if (form.querySelector('#number').value === '') {
-      return alert('you need to put a number');
+    if (form.querySelector("#number").value === "") {
+      return alert("you need to put a number");
     } else if (isIncome === undefined) {
       return alert(
-        'Sorry but you will need to specify if it it an Income or not '
+        "Sorry but you will need to specify if it it an Income or not "
       );
     }
-    const dateValue = form.querySelector('#date').value;
-    const conceptValue = form.querySelector('#concept').value.trim();
+    const dateValue = form.querySelector("#date").value;
+    const conceptValue = form.querySelector("#concept").value.trim();
 
     return sumitHandler(event, isIncome, dateValue, conceptValue);
   };
 
   const changeHandler = (event) => {
     const radioButtonHandler = (event) => {
-      if (event.target.value === 'Income') {
+      if (event.target.value === "Income") {
         return (isIncome = true);
       } else {
         return (isIncome = false);
@@ -35,12 +35,13 @@ const AddPage = ({ sumitHandler }) => {
 
   return (
     <section>
-      <Header></Header>
-      <div className='edit-element'>
+      <Header />
+      <div className="edit-element">
         <AddForm
           needsRadioButton={true}
           click={(event) => clickHandler(event)}
-          change={(event) => changeHandler(event)}></AddForm>
+          change={(event) => changeHandler(event)}
+        />
       </div>
     </section>
   );
