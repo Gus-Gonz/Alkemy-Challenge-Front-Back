e4a2cb741999bf5869310bcde9d704e8707ec541
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-03 10:46:25 -0300

    reorganizacion usando Prettier

--- back-end/models/operation.js
+++ back-end/models/operation.js
@@ -1,29 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = require('../util/database');
+const sequelize = require("../util/database");
 
 /* Creamos nuestro modelo de operaciones con el tipo de 
 datos requerido y su llave primaria para poder identificar
 cada operación univocamente */
 
-const Operation = sequelize.define('operation', {
+const Operation = sequelize.define(
+  "operation",
+  {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
     },
     concept: Sequelize.STRING,
     amount: Sequelize.FLOAT,
     date: Sequelize.DATEONLY,
     type: Sequelize.STRING,
-    category: Sequelize.STRING
-
-},{
+    category: Sequelize.STRING,
+  },
+  {
     timestamps: false,
     freezeTableName: true,
-    tableName: 'operation',
-    classMethods: {}
-});
+    tableName: "operation",
+    classMethods: {},
+  }
+);
 
 module.exports = Operation;
--- back-end/routes/operation-routes.js
+++ back-end/routes/operation-routes.js
@@ -1,21 +1,20 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 
-
-const operationController = require('../controllers/operation-controller');
+const operationController = require("../controllers/operation-controller");
 
 const router = express.Router();
 
-router.get('/', operationController.getOperations);
+router.get("/", operationController.getOperations);
 
-router.get('/last-ten', operationController.getLastTenOperations);
+router.get("/last-ten", operationController.getLastTenOperations);
 
-router.get('/type/:type', operationController.getOperationsByType);
+router.get("/type/:type", operationController.getOperationsByType);
 
-router.post('/', operationController.createOperation);
+router.post("/", operationController.createOperation);
 
-router.put('/:oid', operationController.updateOperation);
+router.put("/:oid", operationController.updateOperation);
 
-router.delete('/:oid', operationController.deleteOperation);
+router.delete("/:oid", operationController.deleteOperation);
 
 module.exports = router;
